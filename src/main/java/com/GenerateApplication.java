c7353cd064f19122bc1a7fb80d26cddb5aa4c74e
lzm <1329835847@qq.com>
2021-07-08 17:26:26 +0800

    nacos 未完成

--- src/main/java/com/GenerateApplication.java
+++ src/main/java/com/GenerateApplication.java
@@ -3,9 +3,11 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication()
-@MapperScan({"com.lzm.mapper"})
+//@EnableDiscoveryClient
+@MapperScan(value = {"com.lzm.mapper"})
 public class GenerateApplication {
 
     public static void main(String[] args) {
